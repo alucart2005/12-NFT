90d786d539939f62635aa8cee63b0b39730d0bc3
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-01-17 19:04:29 -0500

    Feature: Menu Item

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { Logo } from "../pages/logo";
+import { useState } from "react";
 
 export function Navegation() {
+  const [click, setClick] = useState(true);
   return (
     <Section>
       <NavBar>
-        <Logo></Logo>
+        <Logo />
+        <Menu click={click}>
+          <MenuItem>
+          Home</MenuItem>
+        </Menu>
       </NavBar>
     </Section>
   );
@@ -22,4 +28,37 @@ const NavBar = styled.nav`
   width: 85%;
   height: ${(props) => props.theme.navHeight};
   margin: 0 auto;
+`;
+const Menu = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  @media (max-width: 64em) {
+    position: fixed;
+    top: ${(props) => props.theme.navHeight};
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100vw;
+    background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.85)`};
+  }
+`;
+const MenuItem = styled.li`
+  margin: 0 1rem;
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+  font-size: ${(props) => props.theme.fontlg};
+  &::after{
+    content: " ";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${(props)=>props.theme.text};
+    transition: width 0.3s ease;
+  }
+  &:hover::after{
+    width: 100%;
+
+  }
 `;
